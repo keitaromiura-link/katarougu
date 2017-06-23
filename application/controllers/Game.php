7f32b4857db5d51ce5c9e268e1c108e3cf99e907
keitaromiura-link <k.miura@reservelink.co.jp>
2017-06-23 16:51:54 +0900

    gameが取得できない件を確認

--- application/controllers/Game.php
+++ application/controllers/Game.php
@@ -22,7 +22,8 @@ class Game extends CI_Controller {
             //現在のゲームと親とカタログの状況を表示する
             $query = $this->db->get_where('game', array('game_id', $now_game_id), 1);
             $game = $query->row();
-            $query = $this->db->get_where('customer', array('cus_id', $game->game_cus_id_parent), 1);
+            var_dup($game);
+            $query = $this->db->get_where('customer', array('cus_id', $game->game_cus_id_parent ), 1);
             $parant= $query->row();
             $query = $this->db->get_where('catalog', array('cl_id', $game->game_cl_id), 1);
             $catalog= $query->row();
